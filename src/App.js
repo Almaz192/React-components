14002ab71fcee3e5ebe115152ef8657b98359da8
Almaz192 <KOMBIKLAY156@GMAIL.COM>
2024-12-10 12:08:01 +0600

    SSS

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import Button from "./components/button";
 function App() {
   return (
     <div>
+      <Button size ="sm">Confirm1</Button>
       <Button variant="bordered">Confirm</Button>
-      <Button>Cancel</Button>
+      <Button size="lg">Cancel</Button>
     </div>
-  )
+
+  );
 }
 
 export default App;
